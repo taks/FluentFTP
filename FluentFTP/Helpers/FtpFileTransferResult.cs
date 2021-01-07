c6f4fe617b0e06312dacb088d7509e0684dc1838
taks <857tn859@gmail.com>
2021-01-07 19:57:58 +0900

    split FtpFileTransferResult class

--- FluentFTP/Helpers/FtpFileTransferResult.cs
+++ FluentFTP/Helpers/FtpFileTransferResult.cs
@@ -7,7 +7,7 @@ namespace FluentFTP {
 	/// <summary>
 	/// Stores the result of a file transfer when UploadDirectory or DownloadDirectory is used.
 	/// </summary>
-	public class FtpFileTransferResult {
+	public class FtpFileTransferResult : FtpResult {
 
 		/// <summary>
 		/// Returns true if the file was downloaded, false if it was uploaded.
@@ -25,31 +25,6 @@ namespace FluentFTP {
 		public long Size { get; internal set; }
 
 		/// <summary>
-		/// Gets the name and extension of the file.
-		/// </summary>
-		public string Name { get; internal set; }
-
-		/// <summary>
-		/// Stores the absolute remote path of the the current file being transfered.
-		/// </summary>
-		public string RemotePath { get; internal set; }
-
-		/// <summary>
-		/// Stores the absolute local path of the the current file being transfered.
-		/// </summary>
-		public string LocalPath { get; internal set; }
-
-		/// <summary>
-		/// Gets the error that occuring during transfering this file, if any.
-		/// </summary>
-		public Exception Exception { get; internal set; }
-
-		/// <summary>
-		/// Returns true if the file was downloaded/uploaded, or the file was already existing with the same file size.
-		/// </summary>
-		public bool IsSuccess { get; internal set; }
-
-		/// <summary>
 		/// Was the file skipped?
 		/// </summary>
 		public bool IsSkipped { get; internal set; }
@@ -58,11 +33,6 @@ namespace FluentFTP {
 		/// Was the file skipped due to failing the rule condition?
 		/// </summary>
 		public bool IsSkippedByRule { get; internal set; }
-
-		/// <summary>
-		/// Was there an error during transfer? You can read the Exception property for more details.
-		/// </summary>
-		public bool IsFailed { get; internal set; }
 
 		/// <summary>
 		/// Convert this result to a FTP list item.
---
+++ FluentFTP/Helpers/FtpResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentFTP
+{
+	/// <summary>
+	/// TODO:
+	/// </summary>
+	public class FtpResult {
+
+		/// <summary>
+		/// Gets the name and extension of the file.
+		/// </summary>
+		public string Name { get; internal set; }
+
+		/// <summary>
+		/// Stores the absolute remote path of the the current file being transfered.
+		/// </summary>
+		public string RemotePath { get; internal set; }
+
+		/// <summary>
+		/// Stores the absolute local path of the the current file being transfered.
+		/// </summary>
+		public string LocalPath { get; internal set; }
+
+		/// <summary>
+		/// Gets the error that occuring during transfering this file, if any.
+		/// </summary>
+		public Exception Exception { get; internal set; }
+
+		/// <summary>
+		/// Returns true if the file was downloaded/uploaded, or the file was already existing with the same file size.
+		/// </summary>
+		public bool IsSuccess { get; internal set; }
+
+		/// <summary>
+		/// Was there an error during transfer? You can read the Exception property for more details.
+		/// </summary>
+		public bool IsFailed { get; internal set; }
+	}
+}
